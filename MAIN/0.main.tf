6105fd8794c6798110d68ee6926fb4635a187d70
lukeSDZ <lukaszlubnar@gmail.com>
2023-12-30 10:35:02 -0800

    first excercise

--- MAIN/0.main.tf
+++ MAIN/0.main.tf
@@ -2,16 +2,6 @@
 ### TODO: Setup Your provider
 ###
 provider "azurerm" {
-  subscription_id = "4dc4b888-e3bc-4c76-951b-4ece151e3f19"
+  subscription_id = "8151dbff-5649-4c40-8c74-2e861e0b2510"
   features {}
 }
-
-#TODO
-#terraform {
-#  backend "azurerm" {
-#    storage_account_name = "<ACCOUNT NAME>" 
-#    container_name       = "<CONTAINER>" 
-#    key                  = "cloudeng.shared.terraform.tfstate"  
-#    access_key  = 
-#  }
-#}
